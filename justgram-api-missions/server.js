09f5d3e7ff99fba766f47c9ddf33f2d3869e1ac5
soheon-lee <lsheon93@gmail.com>
2022-10-24 11:35:55 +0900

    Refactor: 인메모리 posting제거

--- justgram-api-missions/server.js
+++ justgram-api-missions/server.js
@@ -170,52 +170,6 @@ app.post('/posting', validateToken, async (req, res) => {
 app.get('/postList', validateToken, async (req, res) => {
   // Option 1
   // data 형태를 직접 만들어서, res.json()으로 보내주기
-
-  const postingData = [
-    {
-      'id': 1
-    }, {
-      'id': 2
-    }
-  ]
-
-  res.status(200).json({
-    'data': postingData
-  })
-  res.status(200).json({
-    "data" : [
-      {
-        "userID"           : 1,
-        "userName"         : "Rebekah Johnson",
-        "postingId" : 1,
-        "postingTitle"     : "간단한 HTTP API 개발 시작!",
-        "postingContent"   : "Node.js에 내장되어 있는 http 모듈을 사용해서 HTTP server를 구현."
-      },
-      {  
-        "userID"           : 2,
-        "userName"         : "Fabian Predovic",
-        "postingId"        : 2,
-        "postingTitle"     : "HTTP의 특성",
-        "postingContent"   : "Request/Response와 Stateless!!"
-      },
-      {  
-        "userID"           : 3,
-        "userName"         : "new user 1",
-        "postingId"        : 3,
-        "postingTitle"  : "내용 1",
-        "postingContent"   : "sampleContent3"
-      },
-      {  
-        "userID"           : 4,
-        "userName"         : "new user 2",
-        "postingId"        : 4,
-        "postingTitle"  : "내용 2",
-        "postingContent"   : "sampleContent4"
-      }
-    ]
-  })
-  // Option 2
-  // array를 가공해서 형태에 맞는 데이터를 만든다.
 })
 
 // pseudo-code - 가짜코드
