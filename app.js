dbe708f5fb52fee9a0740d163ac1615f9a040925
Jisoo <k3ji0706@gmail.com>
2022-10-26 00:51:15 +0900

    UNIT test >  FAILED, SUCCESS CASE 1개씩 작업 완료

--- app.js
+++ app.js
@@ -4,21 +4,24 @@ const cors = require('cors');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
 
-const app = express();
+const createApp = () => {
+  const app = express();
 
-app.use(express.json());
-app.use(routes);
-app.use(cors());
-app.use(morgan('combined'));
-app.use(methodOverride());
+  app.use(express.json());
+  app.use(routes);
+  app.use(cors());
+  app.use(morgan('combined'));
+  app.use(methodOverride());
 
-app.use((err, req, res, next) => {
-  const { status, message } = err
-  console.error(err);
-  res.status(status || 500).json({ message });
-})
+  app.use((err, req, res, next) => {
+    const { status, message } = err
+    console.error(err);
+    res.status(status || 500).json({ message });
+  })
+  return app
+}
 
-module.exports = app
+module.exports = {createApp}
 
 
 
--- server.js
+++ server.js
@@ -1,19 +1,15 @@
 const dotenv = require("dotenv");
-dotenv.config();
+dotenv.config()
 
-const PORT = 8000
-const http = require('http');
-const app = require('./app');
-const server = http.createServer(app);
+const { createApp } = require('./app');
 
 const startServer = async () => {
-  try {
-    server.listen(PORT, () => {
-      console.log(`Server is listening on PORT ${PORT}`);
-    })
-  } catch (err) {
-    console.log(err);
-  }
+  const app = createApp();
+  const PORT = process.env.PORT
+
+  app.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`)
+  })
 }
 
 startServer();
