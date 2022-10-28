8cbd13ba38e4c7267b87f0157b56e55362c3071f
Jisoo <k3ji0706@gmail.com>
2022-10-28 14:58:46 +0900

    cors 위치 수정 >> 위치가 라우트보다 아래있으면 에러

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const routes = require('./routes');
 const cors = require('cors');
+const routes = require('./routes');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
 
@@ -8,8 +8,8 @@ const createApp = () => {
   const app = express();
 
   app.use(express.json());
-  app.use(routes);
   app.use(cors());
+  app.use(routes);
   app.use(morgan('combined'));
   app.use(methodOverride());
 
